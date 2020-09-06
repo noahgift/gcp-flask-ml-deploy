80a3c88259803e9ebf8e719f4cab06dcf75d6730
Noah Gift <noah.gift@gmail.com>
2020-09-06 00:31:47 +0000

    adding files for auto-deploy

---
+++ Makefile
@@ -0,0 +1,4 @@
+install-local:
+	pip install -r local-requirements.txt
+install:
+	pip install -r requirements.txt
---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    print("I am inside hello world")
+    return 'Hello World!'
+
+@app.route('/echo/<name>')
+def echo(name):
+    print(f"This was placed in the url: {name}")
+    val = {"name": name}
+    return jsonify(val)
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
