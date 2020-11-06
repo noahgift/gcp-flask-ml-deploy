faa113de68a1e2d5b46098568e468ba6feab20de
Noah Gift <noah.gift@gmail.com>
2020-11-05 23:06:12 -0500

    Testing Continuous Delivery

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello World!'
+    return 'Hello World! CD'
 
 @app.route('/echo/<name>')
 def echo(name):
