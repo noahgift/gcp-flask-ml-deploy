be5e205ffa153693a43f1faaa984c04224d20fac
Kuldeep Kumar <kuldeep.kd@outlook.com>
2023-04-23 16:37:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,12 @@ def echo(name):
     val = {"new-name": name}
     return jsonify(val)
 
+@app.route('/echo/<name>')
+def echo(name):
+    print(f"This was placed in the url: new-{name}")
+    val = {"new-name": name}
+    return jsonify(val)
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
